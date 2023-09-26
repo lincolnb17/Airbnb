0ad8809c1749518cec4ed49f5a9c6ff9e7e39cae
lincolb17 <bhattachanl17@gmail.com>
2023-09-27 00:28:21 +0545

    listing

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -30,7 +30,12 @@ const Map:React.FC<MapProps>=({
                 <TileLayer
                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />  
+                /> 
+                {center &&(
+                    <Marker
+                    position={center as L.LatLngExpression}
+                    />
+                )} 
         </MapContainer>
     )
 
